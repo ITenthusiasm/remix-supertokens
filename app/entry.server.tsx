207875e96ee79f6d3b3f92254275b7400fba25d4
ITenthusiasm <47364027+ITenthusiasm@users.noreply.github.com>
2022-05-02 10:29:53 -0400

    chore: Run Prettier Formatter on Codebase

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -8,9 +8,7 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  let markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
 
   responseHeaders.set("Content-Type", "text/html");
 
